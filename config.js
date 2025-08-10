ea94cad91d2a5d2882632ae4b5860370305db3bd
Fahiye Muhammad <fahiye254@gmail.com>
2025-08-10 22:53:15 +0300

    Remove config.js from tracking

--- config.js
+++
@@ -1,7 +0,0 @@
-export const OMDB_API_KEY = "f9c47ce7";
-export const YOUTUBE_API_KEY = "AIzaSyBTBsIVpo4B3YKfExKsnEolTmKs65c96O0";
-
-
-// export const YOUTUBE_API_KEY = "AIzaSyCbyIO2S8OXkl8g6_RUgQLKF320V-alSVY";
-
-// export const YOUTUBE_API_KEY = "AIzaSyDIzEp_PnMHW-0AIlfEpY-uS8HwtfvaB3c"
